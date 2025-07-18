61095cef0c98edfa140187890d7e74e19bbc6a57
Hector Lliguichuzhca <lliguichuzcah@gmail.com>
2025-07-18 10:48:00 -0400

    Refactor files location addded RootView and MainView in SwiftUI

---
+++ WalletBuddy/WalletBuddy/Views/SwiftUi/RootView.swift
@@ -0,0 +1,22 @@
+//
+//  RootView.swift
+//  WalletBuddy
+//
+//  Created by Hector Lliguichuzca on 7/18/25.
+//
+import SwiftUI
+
+struct RootView: View {
+    @ObservedObject var appViewModel: AppViewModel
+
+    var body: some View {
+        switch appViewModel.state {
+        case .loggedOut:
+            Text("Logged Out")
+        case .loadingSkeleton:
+            SkeletonView()
+        case .loggedIn:
+            MainView()
+        }
+    }
+}
